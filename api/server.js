0a3411b23b30218aaee52d3ed0a9e2882de3c21d
Chris Kelley <chriskelley85@gmail.com>
2022-03-07 16:33:53 -0600

    users post

--- api/server.js
+++ api/server.js
@@ -1,4 +1,21 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
 const server = express();
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+// module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const Model = require('./users/model')
+server.use(express.json());
+
+server.post('/api/users', async (req, res) => {
+    try {
+        const { name, bio } = req.body
+        const newUser = await Model.insert({name, bio})
+        if(!name || !bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        } else {
+            res.status(201).json(newUser)
+        }
+    } catch(err) {
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+    }
+})
+module.exports = server;
